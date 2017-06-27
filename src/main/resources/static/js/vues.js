028d5ff3883d0796c7d2b14606b3460f684837a4
RastegaevYO <RastegaevYO@sovcombank.ru>
2017-06-27 14:38:13 +0400

    dinamic component rendering

--- src/main/resources/static/js/vues.js
+++ src/main/resources/static/js/vues.js
@@ -2,11 +2,21 @@
  * Created by User on 20.06.2017.
  */
 
-function initVue() {
+Vue.component('text-field', {
+    template: '<div>text</div>'
+});
 
-}
+Vue.component('datetime-field', {
+    template: '<div>datetime</div>'
+});
 
-initVue()
+Vue.component('fixedchoice-field', {
+    template: '<div>fixedchoice</div>'
+});
+
+Vue.component('office-field', {
+    template: '<div>office</div>'
+});
 
 
 var vm = new Vue({
@@ -14,28 +24,31 @@ var vm = new Vue({
     data: {
         questions: '',
         currentQuestion: 0,
-        prevQuestions: []
+        prevQuestions: [],
+        fieldComponents: ['text-field', 'office-field']
 
     },
     created: function () {
-        var xhr = new XMLHttpRequest()
-        var self = this
-        xhr.open('GET', '/json')
+        var xhr = new XMLHttpRequest();
+        var self = this;
+        xhr.open('GET', '/json');
         xhr.onload = function () {
-            self.questions = JSON.parse(xhr.responseText)
+            self.questions = JSON.parse(xhr.responseText);
             console.log(self.questions[0])
-        }
+        };
         xhr.send()
     },
 
     methods: {
-        switchQuestion: function (nextQuestionId,answerId) {
-            var pq={}
-            pq.qId=this.currentQuestion
-            pq.aId=answerId
+        switchQuestion: function (nextQuestionId, answerId) {
+            var pq = {};
+            pq.qId = this.currentQuestion;
+            pq.aId = answerId;
 
-            this.prevQuestions.push(pq)
-            this.currentQuestion = nextQuestionId
+            this.prevQuestions.push(pq);
+            this.currentQuestion = nextQuestionId;
+
+            this.fieldComponents.push('fixedchoice-field');
         }
     }
-})
+});
